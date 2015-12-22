derhass/dxx-rebirth

commit 5b9e50a815c362bf67c2e09866c92db03b40f4dd

Use inline namespace dcx for common/misc

common/include/dxxerror.h
@@ -39,6 +39,8 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #define __noreturn
 #endif
 
+inline namespace dcx {
+
 int error_init(void (*func)(const char *));    //init error system, returns 0=ok
 void Warning_puts(const char *str) __attribute_nonnull();
 void Warning(const char *fmt,...) __attribute_format_printf(1, 2);				//print out warning message to user
@@ -113,6 +115,8 @@ static inline void d_debugbreak()
 #ifdef DXX_DEBUGBREAK_TRAP
 	DXX_DEBUGBREAK_TRAP();
 #endif
+
+}
 
 }
 

common/include/hash.h
@@ -18,12 +18,11 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 */
 
 
+#pragma once
 
-#ifndef _HASH_H
-#define _HASH_H
-
-#ifdef __cplusplus
 #include <map>
+
+inline namespace dcx {
 
 struct hashtable
 {
@@ -37,6 +36,4 @@ struct hashtable
 int hashtable_search( hashtable *ht, const char *key );
 void hashtable_insert( hashtable *ht, const char *key, int value );
 
-#endif
-
-#endif
+}

common/include/hmp.h
@@ -16,6 +16,7 @@
 #include "physfsx.h"
 
 #ifdef __cplusplus
+inline namespace dcx {
 
 #define HMP_TRACKS 32
 #ifdef _WIN32
@@ -101,6 +102,7 @@ void hmp_resume(hmp_file *hmp);
 void hmp_reset();
 #endif
 
+}
 #endif
 
 #endif

common/include/ignorecase.h
@@ -54,6 +54,8 @@
 
 #ifdef __cplusplus
 
+inline namespace dcx {
+
 /**
  * \fn int PHYSFSEXT_locateCorrectCase(char *buf)
  * \brief Find an existing filename with matching case.
@@ -81,6 +83,8 @@
 int PHYSFSEXT_locateCorrectCase(char *buf);
 
 /* end of ignorecase.h ... */
+
+}
 
 #endif
 

common/include/strutil.h
@@ -13,6 +13,8 @@
 #include "dxxsconf.h"
 #include <vector>
 
+inline namespace dcx {
+
 #if defined(macintosh)
 #define snprintf macintosh_snprintf
 extern void snprintf(char *out_string, int size, const char * format, ... );
@@ -79,5 +81,7 @@ public:
 };
 
 int string_array_sort_func(char **e0, char **e1);
+
+}
 
 #endif

common/misc/error.cpp
@@ -31,6 +31,8 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "console.h"
 #include "dxxerror.h"
 
+inline namespace dcx {
+
 #define MAX_MSG_LEN 2048
 
 static void (*ErrorPrintFunc)(const char *);
@@ -147,4 +149,6 @@ int error_init(void (*func)(const char *))
 {
 	ErrorPrintFunc = func;          // Set Error Print Functions
 	return 0;
+}
+
 }

common/misc/hash.cpp
@@ -25,6 +25,8 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include <string.h>
 #include "hash.h"
 
+inline namespace dcx {
+
 bool hashtable::compare_t::operator()(const char *l, const char *r) const
 {
 	for (;; ++l, ++r)
@@ -48,4 +50,6 @@ int hashtable_search(hashtable *ht, const char *key)
 void hashtable_insert(hashtable *ht, const char *key, int value)
 {
 	ht->m.insert(std::make_pair(key, value));
+}
+
 }

common/misc/hmp.cpp
@@ -27,6 +27,8 @@
 #include "compiler-range_for.h"
 #include "partial_range.h"
 
+inline namespace dcx {
+
 #define MIDIINT(x)	(words_bigendian ? (x) : (SWAPINT(x)))
 #define MIDISHORT(x)	(words_bigendian ? (x) : (SWAPSHORT(x)))
 
@@ -711,4 +713,6 @@ void hmp2mid(const char *hmp_name, std::vector<uint8_t> &midbuf)
 		be_bytebuffer_t bbmi(&midbuf[midtrklenpos]);
 		serial::process_buffer(bbmi, static_cast<int32_t>(size_after - size_before));
 	}
+}
+
 }

commit dcff4b272b12dd8cddc0d837a90ea5ba68da286e

fix inline namespace dcx for non-x86/non-x64 builds
On non-x86/x64 platforms, common/include/dxxerror.h might #include <signal.h>
for debug builds. The commit 5b9e50a815c362bf67c2e09866c92db03b40f4dd
added a namespcae dcx {} block around that, resulting in some users of this
macro not finding the correct definition for struct sigevent.

This workaround just moves the whole DXX_DEBUGBREAK_TRAP related macros
out of the namespace.

Fixes: 5b9e50a815c362bf67c2e09866c92db03b40f4dd

common/include/dxxerror.h
@@ -39,6 +39,34 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #define __noreturn
 #endif
 
+/* Allow macro override */
+
+#if defined(DXX_ENABLE_DEBUGBREAK_TRAP) && !defined(DXX_DEBUGBREAK_TRAP)
+
+#if defined __clang__
+	/* Must be first, since clang also defines __GNUC__ */
+#	define DXX_DEBUGBREAK_TRAP()	__builtin_debugtrap()
+#elif defined __GNUC__
+#	if defined(__i386__) || defined(__amd64__)
+#		define DXX_DEBUGBREAK_TRAP()	__asm__ __volatile__("int3" ::: "cc", "memory")
+#	endif
+#elif defined _MSC_VER
+#	define DXX_DEBUGBREAK_TRAP()	__debugbreak()
+#endif
+
+#ifndef DXX_DEBUGBREAK_TRAP
+#	if defined __unix__
+/* for raise */
+#		include <signal.h>
+#		define DXX_DEBUGBREAK_TRAP()	raise(SIGTRAP)
+#	elif defined __GNUC__
+	/* May terminate execution */
+#		define DXX_DEBUGBREAK_TRAP()	__builtin_trap()
+#	endif
+#endif
+
+#endif
+
 namespace dcx {
 
 int error_init(void (*func)(const char *));    //init error system, returns 0=ok
@@ -75,34 +103,6 @@ void UserError(const char *fmt, ...) __noreturn __attribute_format_printf(1, 2);
 
 #ifndef NDEBUG		//macros for debugging
 #	define DXX_ENABLE_DEBUGBREAK_TRAP
-#endif
-
-/* Allow macro override */
-
-#if defined(DXX_ENABLE_DEBUGBREAK_TRAP) && !defined(DXX_DEBUGBREAK_TRAP)
-
-#if defined __clang__
-	/* Must be first, since clang also defines __GNUC__ */
-#	define DXX_DEBUGBREAK_TRAP()	__builtin_debugtrap()
-#elif defined __GNUC__
-#	if defined(__i386__) || defined(__amd64__)
-#		define DXX_DEBUGBREAK_TRAP()	__asm__ __volatile__("int3" ::: "cc", "memory")
-#	endif
-#elif defined _MSC_VER
-#	define DXX_DEBUGBREAK_TRAP()	__debugbreak()
-#endif
-
-#ifndef DXX_DEBUGBREAK_TRAP
-#	if defined __unix__
-/* for raise */
-#		include <signal.h>
-#		define DXX_DEBUGBREAK_TRAP()	raise(SIGTRAP)
-#	elif defined __GNUC__
-	/* May terminate execution */
-#		define DXX_DEBUGBREAK_TRAP()	__builtin_trap()
-#	endif
-#endif
-
 #endif
 
 // Encourage optimizer to treat d_debugbreak paths as unlikely