derhass/dxx-rebirth

commit d4857f42579bcbb7e35ff1539da71e284628c8f6

Use inline namespace dsx for similar/3d

common/include/interp.h
@@ -26,6 +26,7 @@ const std::size_t MAX_POLYGON_VECS = 1000;
 struct polygon_model_points : array<g3s_point, MAX_POLYGON_VECS> {};
 
 #if defined(DXX_BUILD_DESCENT_I) || defined(DXX_BUILD_DESCENT_II)
+inline namespace dsx {
 #if defined(DXX_BUILD_DESCENT_I)
 static const size_t glow_array_size = 1;
 #elif defined(DXX_BUILD_DESCENT_II)
@@ -38,10 +39,11 @@ struct glow_values_t : public array<fix, glow_array_size> {};
 //calls the object interpreter to render an object.  The object renderer
 //is really a seperate pipeline. returns true if drew
 void g3_draw_polygon_model(const uint8_t *model_ptr,grs_bitmap **model_bitmaps,submodel_angles anim_angles,g3s_lrgb light,const glow_values_t *glow_values, polygon_model_points &Interp_point_list);
-#endif
 
 //init code for bitmap models
 int16_t g3_init_polygon_model(void *model_ptr);
+}
+#endif
 
 //un-initialize, i.e., convert color entries back to RGB15
 static inline void g3_uninit_polygon_model(void *model_ptr)
@@ -49,15 +51,15 @@ static inline void g3_uninit_polygon_model(void *model_ptr)
 	(void)model_ptr;
 }
 
+#if defined(DXX_BUILD_DESCENT_I) || defined(DXX_BUILD_DESCENT_II)
+inline namespace dsx {
 //alternate interpreter for morphing object
 void g3_draw_morphing_model(const uint8_t *model_ptr,grs_bitmap **model_bitmaps,submodel_angles anim_angles,g3s_lrgb light,const vms_vector *new_points, polygon_model_points &Interp_point_list);
 
-//this remaps the 15bpp colors for the models into a new palette.  It should
-//be called whenever the palette changes
-void g3_remap_interp_colors(void);
-
 // check a polymodel for it's color and return it
 int g3_poly_get_color(const uint8_t *model_ptr);
+}
+#endif
 
 #ifdef WORDS_BIGENDIAN
 // routine to convert little to big endian in polygon model data

common/include/3d.h
@@ -36,7 +36,9 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 struct grs_bitmap;
 
 #ifdef EDITOR
+inline namespace dcx {
 extern int g3d_interp_outline;      //if on, polygon models outlined in white
+}
 #endif
 
 //Structure for storing u,v,light values.  This structure doesn't have a

common/main/polyobj.h
@@ -36,10 +36,12 @@ struct bitmap_index;
 #include <physfs.h>
 #include "pack.h"
 
-struct glow_values_t;
 struct robot_info;
 
 #if defined(DXX_BUILD_DESCENT_I) || defined(DXX_BUILD_DESCENT_II)
+inline namespace dsx {
+struct glow_values_t;
+}
 #if defined(DXX_BUILD_DESCENT_I)
 #define MAX_POLYGON_MODELS 85
 #elif defined(DXX_BUILD_DESCENT_II)

similar/3d/interp.cpp
@@ -23,6 +23,8 @@
 #include "byteutil.h"
 #include "u_mem.h"
 
+inline namespace dcx {
+
 static const unsigned OP_EOF = 0;   //eof
 static const unsigned OP_DEFPOINTS = 1;   //defpoints
 static const unsigned OP_FLATPOLY = 2;   //flat-shaded polygon
@@ -33,11 +35,15 @@ static const unsigned OP_SUBCALL = 6;   //call a subobject
 static const unsigned OP_DEFP_START = 7;   //defpoints with start
 static const unsigned OP_GLOW = 8;   //glow value for next poly
 
-static int16_t init_model_sub(uint8_t *p, int16_t);
-
 #ifdef EDITOR
 int g3d_interp_outline;
 #endif
+
+}
+
+inline namespace dsx {
+
+static int16_t init_model_sub(uint8_t *p, int16_t);
 
 #if defined(DXX_BUILD_DESCENT_I) || defined(WORDS_BIGENDIAN)
 static inline int16_t *wp(uint8_t *p)
@@ -781,4 +787,6 @@ int16_t g3_init_polygon_model(void *model_ptr)
 	#endif
 
 	return init_model_sub(reinterpret_cast<uint8_t *>(model_ptr), -1);
+}
+
 }

commit 9f3ef389055c05455f30459ecf80df046a5c42af

change namespace dcx to dsx in common/include/interp.h
Some functions only present in the WORDS_BIGENDIAN and WORDS_NEED_ALIGNMENT
case were incorrectly declared in namespace dcx in the header, while they
were defined in namepsace dsx in similiar/3d/interp.cpp. This broke the
RPi build.

Fixes: d4857f42579bcbb7e35ff1539da71e284628c8f6

common/include/interp.h
@@ -63,7 +63,7 @@ int g3_poly_get_color(const uint8_t *model_ptr);
 }
 #endif
 
-namespace dcx {
+namespace dsx {
 #ifdef WORDS_BIGENDIAN
 // routine to convert little to big endian in polygon model data
 void swap_polygon_model_data(ubyte *data);